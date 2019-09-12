5403d4be8789732a91cd209de0cb6dc60ec4c21a
石朋 <tgbrfvedcs1@gmail.com>
2019-09-12 22:31:55 +0800

    final

--- index.js
+++ index.js
@@ -1,70 +1,77 @@
-const querystring = require('querystring');
-const https = require('https');
+const MailChimp = require('./mailchimp');
+const {
+  sampleTemplate,
+  sampleList,
+  sampleCampaign,
+  sampleMembers
+} = require('./sampleDatas');
 
-const url = 'https://us4.api.mailchimp.com/3.0/';
-const apikey = '3609d8d80608b4c45305dbbde64a1a2f-us4';
+const apiKey = 'api-key'; // Get this from your account on mailchimp.com
 
-const createList = () => {
-  let qs =
-    '{"name":"Freddie\'s Favorite Hats","contact":{"company":"Mailchimp","address1":"675 Ponce De Leon Ave NE","address2":"Suite 5000","city":"Atlanta","state":"GA","zip":"30308","country":"US","phone":""},"permission_reminder":"You\'re receiving this email because you signed up for updates about Freddie\'s newest hats.","campaign_defaults":{"from_name":"Freddie","from_email":"freddie@freddiehats.com","subject":"","language":"en"},"email_type_option":true}';
-  let option = {
-    auth: 'user:' + apikey,
-    method: 'POST',
-    header: { 'content-type': 'application/json' }
-  };
+const mailchimp = new MailChimp(apiKey);
 
-  const req = https.request(url, option, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    res.on('data', chunk => {
-      let reply = JSON.parse(chunk);
-      debugger;
-    });
-  });
-
-  req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
-  });
-  req.write(data);
-  req.end();
+const createList = async () => {
+  //create list
+  let result = await mailchimp.createList(sampleList);
+  console.log(x ? 'Created' : 'Fail');
+  return [result.id];
 };
-createList();
 
-// {
-//     name: 'bar',
-//     contact: {
-//       company: 's',
-//       address1: 'eryjkfwef',
-//       city: 'taiepi',
-//       state: 'goin',
-//       zip: '106',
-//       country: 'taiwan'
-//     },
-//     permission_reminder: 'reminder',
-//     campaign_defaults: {
-//       from_name: 'casey',
-//       from_email: 'tgbrfv@gmail.com',
-//       subject: 'party night',
-//       language: 'ENG'
-//     },
-//     email_type_option: true
-//   }
-// const https = require('https');
-// const options = {
-//   hostname: 'us4.api.mailchimp.com/3.0/',
-//   path: '/todos',
-//   method: 'GET'
-// };
+const getLists = async () => {
+  let result = await mailchimp.getList();
+  if (result.title) {
+    throw new Error(result.detail);
+  }
+  console.log('Lists: ');
+  result.lists.forEach((list, index) => {
+    console.log(
+      `${index}: ID: ${list.id}  Name: ${list.name}  Date_created: ${list.date_created}`
+    );
+  });
+  return result.lists;
+};
 
-// const req = https.request(options, res => {
-//   console.log(`statusCode: ${res.statusCode}`);
+const addMembers = async listID => {
+  //add members to a list
+  console.log(
+    (await mailchimp.addMember(listID, sampleMembers)) ? 'Member added' : 'Fail'
+  );
+};
 
-//   res.on('data', d => {
-//     process.stdout.write(d);
-//   });
-// });
+const createTemplate = async () => {
+  let template = await mailchimp.createTemplate(sampleTemplate);
+  if (template) {
+    sampleCampaign.settings.template_id = template.id;
+  }
+  console.log('Template created');
+};
 
-// req.on('error', error => {
-//   console.error('s' + error);
-// });
+const createCampaign = async () => {
+  //create campaign
+  let campaign = await mailchimp.createCampaigns(sampleCampaign);
+  console.log('Campaign created');
+  return campaign.id;
+};
 
-// req.end();
+//main
+(async () => {
+  try {
+    let lists = await getLists();
+    if (lists.length === 0) {
+      lists = await createList();
+    }
+
+    //Provide a list for creatiing a campaign later
+    sampleCampaign.recipients.list_id = lists[0].id;
+
+    await addMembers(lists[0].id);
+    await createTemplate();
+    let campaignID = await createCampaign();
+
+    console.log(
+      (await mailchimp.sendCampaigns(campaignID)) ? 'Campaign sended' : 'Fail'
+    );
+  } catch (e) {
+    console.log(e);
+  }
+})();
---
+++ mailchimp.js
@@ -0,0 +1,120 @@
+const https = require('https');
+
+module.exports = class MailService {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+  }
+  //Settings for making https request
+  getOptions = (path, method, dataLength) => {
+    return {
+      hostname: 'us4.api.mailchimp.com',
+      port: 443,
+      auth: 'user:' + this.apiKey,
+      path: '/3.0/' + path,
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': dataLength !== undefined ? dataLength : null
+      }
+    };
+  };
+  request = (options, data) => {
+    //Return response from mailchimp with GET method
+    if (options.method === 'GET') {
+      return new Promise((resolve, reject) => {
+        const req = https.request(options, res => {
+          req.on('error', error => {
+            console.error(error);
+          });
+
+          res.on('data', d => {
+            if (res.statusCode !== 200) {
+              console.error(`statusCode: ${res.statusCode}`);
+              reject(JSON.parse(d));
+            }
+            resolve(JSON.parse(d));
+          });
+        });
+
+        req.end();
+      });
+    } else if (options.method === 'POST') {
+      //return response from mailchimp if statusCode = 200,
+      //otherwise return false.
+      return new Promise((resolve, reject) => {
+        const req = https.request(options, res => {
+          req.on('error', error => {
+            reject(error);
+          });
+
+          res.on('data', d => {
+            let result = JSON.parse(d);
+            if (res.statusCode !== 200) {
+              console.error(`statusCode: ${res.statusCode}`);
+              resolve(result);
+            }
+            resolve(result);
+          });
+        });
+
+        req.write(data);
+        req.end();
+      });
+    }
+  };
+
+  getList = () => {
+    const options = this.getOptions('lists', 'GET');
+    return this.request(options);
+  };
+
+  getCampaigns = () => {
+    const options = this.getOptions('campaigns', 'GET');
+    return this.request(options);
+  };
+
+  getTemplates = () => {
+    const options = this.getOptions('templates', 'GET');
+    return this.request(options);
+  };
+
+  createList = listInfo => {
+    let data = JSON.stringify(listInfo);
+    const options = this.getOptions('lists', 'POST', data.length);
+    return this.request(options, data);
+  };
+
+  addMember = (listID, members) => {
+    let data = JSON.stringify(members);
+    const options = this.getOptions('lists/' + listID, 'POST', data.length);
+    return this.request(options, data);
+  };
+
+  createTemplate = data => {
+    data = JSON.stringify(data);
+    const options = this.getOptions('templates', 'POST', data.length);
+    return this.request(options, data);
+  };
+
+  createCampaigns = data => {
+    data = JSON.stringify(data);
+    const options = this.getOptions('campaigns', 'POST', data.length);
+    return this.request(options, data);
+  };
+
+  sendCampaigns = campaignID => {
+    const options = this.getOptions(
+      'campaigns/' + campaignID + '/actions/send',
+      'POST',
+      0
+    );
+    return new Promise((resolve, reject) => {
+      const req = https.request(options, res => {});
+      req.on('error', error => {
+        reject(error);
+      });
+      resolve(true);
+      req.end();
+    });
+  };
+};
